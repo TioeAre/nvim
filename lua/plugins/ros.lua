15ca64d647fdb936c4021776b949f0a284126272
TioeAre <are10032@outlook.com>
2024-10-06 11:10:58 -0400

    disable ros

--- lua/plugins/ros.lua
+++ lua/plugins/ros.lua
@@ -7,14 +7,14 @@ return {
     --     dependencies = { "nvim-lua/plenary.nvim" },
     --     -- event = "VeryLazy",
     -- },
-    {
-        "thibthib18/ros-nvim",
-        config = M.config_ros_nvim_thibthib18,
-        event = {"VeryLazy", "BufReadPost"},
-    },
-    {
-        "taketwo/vim-ros",
-        config = M.config_vim_ros,
-        event = {"VeryLazy", "BufReadPost"},
-    },
+    -- {
+    --     "thibthib18/ros-nvim",
+    --     config = M.config_ros_nvim_thibthib18,
+    --     event = {"VeryLazy", "BufReadPost"},
+    -- },
+    -- {
+    --     "taketwo/vim-ros",
+    --     config = M.config_vim_ros,
+    --     event = {"VeryLazy", "BufReadPost"},
+    -- },
 }
