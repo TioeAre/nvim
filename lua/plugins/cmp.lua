8c5bd73a55b1de64615cc7737073405c8637861d
TioeAre <are10032@outlook.com>
2025-11-07 22:59:05 +0800

    fix(cmp): specify version for LuaSnip dependency

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -22,6 +22,7 @@ return {
 				"saadparwaiz1/cmp_luasnip",
 				dependencies = {
 					"L3MON4D3/LuaSnip",
+					version = "v2.*",
 					build = "make install_jsregexp",
 					dependencies = "rafamadriz/friendly-snippets",
 				},
